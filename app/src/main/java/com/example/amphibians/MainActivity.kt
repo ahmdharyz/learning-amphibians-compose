2dbd3e042698e70351c811daf5203b1cacd96469
ahmdharyz <22096691+ahmdharyz@users.noreply.github.com>
2025-09-11 20:30:50 +1000

    Add HomeScreen and AmphibiansApp Composables

--- app/src/main/java/com/example/amphibians/MainActivity.kt
+++ app/src/main/java/com/example/amphibians/MainActivity.kt
@@ -7,10 +7,10 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.amphibians.ui.AmphibiansApp
 import com.example.amphibians.ui.theme.AmphibiansTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,28 +20,17 @@ class MainActivity : ComponentActivity() {
         setContent {
             AmphibiansTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    AmphibiansApp(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     AmphibiansTheme {
-        Greeting("Android")
+        AmphibiansApp()
     }
 }
---
+++ app/src/main/java/com/example/amphibians/ui/AmphibiansApp.kt
@@ -0,0 +1,10 @@
+package com.example.amphibians.ui
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import com.example.amphibians.ui.screens.HomeScreen
+
+@Composable
+fun AmphibiansApp(modifier: Modifier = Modifier) {
+    HomeScreen(modifier = Modifier)
+}
