77fda710a4b2d7b0a55ddf1d0994c6b2edad5b05
ahmdharyz <22096691+ahmdharyz@users.noreply.github.com>
2025-09-12 09:35:11 +1000

    Fix error in Gradle

--- app/src/main/java/com/example/amphibians/MainActivity.kt
+++ app/src/main/java/com/example/amphibians/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.consumeWindowInsets
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
@@ -20,7 +21,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AmphibiansTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    AmphibiansApp(modifier = Modifier.padding(innerPadding))
+                    AmphibiansApp(modifier = Modifier.consumeWindowInsets(innerPadding))
                 }
             }
         }
