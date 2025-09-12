9194ff5142fb8bb3f556c01a9173767e6966e86f
ahmdharyz <22096691+ahmdharyz@users.noreply.github.com>
2025-09-12 10:18:24 +1000

    Add UI layer: ViewModel and UiState

---
+++ app/src/main/java/com/example/amphibians/ui/screens/AmphibiansViewModel.kt
@@ -0,0 +1,18 @@
+package com.example.amphibians.ui.screens
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import com.example.amphibians.network.Amphibian
+
+sealed interface AmphibiansUiState {
+    data class Success(val amphibians: List<Amphibian>) : AmphibiansUiState
+    object Error: AmphibiansUiState
+    object Loading: AmphibiansUiState
+}
+
+class AmphibiansViewModel : ViewModel() {
+    var amphibiansUiState: AmphibiansUiState by mutableStateOf(AmphibiansUiState.Loading)
+
+}
