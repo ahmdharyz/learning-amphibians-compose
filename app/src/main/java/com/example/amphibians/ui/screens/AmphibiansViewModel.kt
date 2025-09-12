0c9446e6d152ba745524fc3aaf1babddc296e7fd
ahmdharyz <22096691+ahmdharyz@users.noreply.github.com>
2025-09-12 12:01:38 +1000

    Call the web service in ViewModel

--- app/src/main/java/com/example/amphibians/ui/screens/AmphibiansViewModel.kt
+++ app/src/main/java/com/example/amphibians/ui/screens/AmphibiansViewModel.kt
@@ -4,7 +4,11 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.amphibians.network.Amphibian
+import com.example.amphibians.network.AmphibiansApi
+import kotlinx.coroutines.launch
+import retrofit2.http.GET
 
 sealed interface AmphibiansUiState {
     data class Success(val amphibians: List<Amphibian>) : AmphibiansUiState
@@ -14,5 +18,16 @@ sealed interface AmphibiansUiState {
 
 class AmphibiansViewModel : ViewModel() {
     var amphibiansUiState: AmphibiansUiState by mutableStateOf(AmphibiansUiState.Loading)
+
+    init {
+        getAmphibians()
+    }
+
+    private fun getAmphibians() {
+        viewModelScope.launch {
+            val listResult = AmphibiansApi.retrofitService.getAmphibians()
+            amphibiansUiState = listResult as AmphibiansUiState
+        }
+    }
 
 }
