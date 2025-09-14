4b175783e15d6c3d20aac4ac351a7ba97d67cf8a
ahmdharyz <22096691+ahmdharyz@users.noreply.github.com>
2025-09-14 21:05:08 +1000

    Add loading screen and error screen UI

--- app/src/main/java/com/example/amphibians/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/amphibians/ui/screens/HomeScreen.kt
@@ -1,12 +1,18 @@
 package com.example.amphibians.ui.screens
 
+import com.example.amphibians.R
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
 import com.example.amphibians.network.Amphibian
 
 @Composable
@@ -32,5 +38,32 @@ fun ResultScreen(amphibians: String, modifier: Modifier = Modifier) {
         modifier = modifier
     ) {
         Text(text = amphibians)
+    }
+}
+
+@Composable
+fun LoadingScreen(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+    ) {
+        Image(
+            modifier = modifier.size(200.dp),
+            painter = painterResource(id = R.drawable.loading_img),
+            contentDescription = stringResource(R.string.loading)
+        )
+    }
+}
+
+@Composable
+fun ErrorScreen(modifier: Modifier = Modifier) {
+    Box(
+        contentAlignment = Alignment.Center,
+        modifier = modifier
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_connection_error),
+            contentDescription = stringResource(R.string.loading_failed)
+        )
     }
 }
